daa59bd0b506fee0c785c800ecb2055ee4e2c62a
AksharaGandrakota <89545812+AksharaGandrakota@users.noreply.github.com>
2021-08-25 15:28:56 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*
+* Hello.java
+* Akshara Gandrakota
+* August 25, 2021
+* 255 Introduction to Programming 
+*/
+
+public class Hello {
+	public static void main(String[] args) {
+
+	System.out.println("Akshara Gandrakota");
+	System.out.println("Hello");
+	System.out.println("Lab01");
+	System.out.println("August 25, 2021");
+	System.out.println("255 Introduction to Programming");
+	System.out.println();
+
+	}
+} 
